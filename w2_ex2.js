bf9ff600c5c18529a6dc8b60d2786c4d32e0283f
nobleOn3 <nob16001@mybyui.edu>
2020-01-16 15:01:57 -0700

    Added a function to print out a random D&D sentence

--- w2_ex2.js
+++ w2_ex2.js
@@ -15,11 +15,37 @@ function console_triangle() {
  *******************************************************/
 function fizz_buzz() {
     for(i = 1; i < 101; i++){
-        if((i % 3) == 0)
+        if((i % 3) == 0 && (i % 5) != 0)
             console.log("Fizz");
         else if ((i % 5) == 0 && (i % 3) != 0)
             console.log("Buzz");
+        else if ((i % 5) == 0 && (i % 3) == 0)
+            console.log("FizzBuzz");
         else
             console.log(i);
     }
+}
+
+let cls = ["fighter", "barbarian", "wizard", "warlock", "cleric", "druid"];
+let bbegs = ["Lich", "Beholder", "Red Dragon", "Pit Fiend", "Demon Prince"];
+let weapons = ["Bow of Truth", "Axe of Souls", "Throatripper", "Wand of Fireballs"];
+let races = ["Halfling", "Elf", "Half-Elf", "Half-Orc", "Dragonborn", "Tabaxi", "Human"]
+/**************************************************************************
+ * This function finds a random number at or below the number passed in.
+**************************************************************************/
+function random(num) {
+    return Math.floor(Math.random() * num);
+}
+
+/**************************************************************************
+ * This function builds off of the example from class. It builds a random
+ * sentence that tells about a legendary hero striking down the big bad
+ * evil guy.
+**************************************************************************/
+function dnd_sentence() {
+    let cl = cls[random(6)];
+    let bbeg = bbegs[random(5)];
+    let weapon = weapons[random(4)];
+    let race = races[random(7)];
+    console.log("The " + race + " " + cl + " struck down the "+ bbeg + " using the legendary " + weapon + "!");
 }
