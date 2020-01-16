e105c2d880ce59f6b83bd1f79edb9df24b1fe7cb
nobleOn3 <nob16001@mybyui.edu>
2020-01-16 13:27:23 -0700

    Added a few more files to begin, and a button that logs a triangle!

---
+++ w2_ex2.js
@@ -0,0 +1,10 @@
+/***********************************************************
+ * This function prints a right triangle, using hashtags, 
+ * to the console. 
+ **********************************************************/
+function console_triangle() {
+    str = ""
+    console.log("CLICK")
+    for(i = 0; i < 7; i++)
+        console.log(str +="#");
+}
