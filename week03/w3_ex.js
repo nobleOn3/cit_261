788d2516dfdbd8dff2e9a83821284809dac5b063
nobleOn3 <nob16001@mybyui.edu>
2020-01-25 19:20:32 -0700

    Added a function that can build tables and functions to build both a table of mountains and a table of games.

--- week03/w3_ex.js
+++ week03/w3_ex.js
@@ -26,3 +26,62 @@ let user = {
 function sayHi(){
     user.sayHello();
 }
+
+const MOUNTAINS = [
+    {name: "Kilimanjaro", height: 5895, place: "Tanzania"},
+    {name: "Everest", height: 8848, place: "Nepal"},
+    {name: "Mount Fuji", height: 3776, place: "Japan"},
+    {name: "Vaalserberg", height: 323, place: "Netherlands"},
+    {name: "Denali", height: 6168, place: "United States"},
+    {name: "Popocatepetl", height: 5465, place: "Mexico"},
+    {name: "Mont Blanc", height: 4808, place: "Italy/France"}
+  ];
+
+  const GAMES = [
+      {name: "Pathfinder: Kingseeker", Price: 15.00, approxPlaytime: "4 hours"},
+      {name: "Doom 2015", Price: 5.99, approxPlaytime: "2 hours"},
+      {name: "Monster Hunter: World", Price: 20.00, approxPlaytime: "78 hours"},
+      {name: "Rocket League", Price: 20.00, approxPlaytime: "80 hours"},
+      {name: "CupHead", Price: 15.99, approxPlaytime: "23 hours"}
+  ];
+
+/***********************************************************
+ * This function builds a table which is then displayed
+ * To the user in the "mntns" tag.
+ **********************************************************/
+  function buildTable(data) {
+    let table = document.createElement("table");
+  
+    let fields = Object.keys(data[0]);
+    let headRow = document.createElement("tr");
+    fields.forEach(function(field) {
+      let headCell = document.createElement("th");
+      headCell.appendChild(document.createTextNode(field));
+      headRow.appendChild(headCell);
+    });
+    table.appendChild(headRow);
+
+    data.forEach(function(object) {
+      let row = document.createElement("tr");
+      fields.forEach(function(field) {
+        let cell = document.createElement("td");
+        cell.appendChild(document.createTextNode(object[field]));
+        if (typeof object[field] == "number") {
+          cell.style.textAlign = "right";
+        }
+        row.appendChild(cell);
+      });
+      table.appendChild(row);
+    });
+    console.log(table);
+
+    return table;
+  }
+
+  function buildMntns() {
+    document.querySelector("#mntns").appendChild(buildTable(MOUNTAINS));
+  }
+
+  function buildGames() {
+    document.querySelector("#gms").appendChild(buildTable(GAMES));
+  }
