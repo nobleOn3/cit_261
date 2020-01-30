70c2c8f4a65809d201de43795c05cd7c5057d2ce
nobleOn3 <nob16001@mybyui.edu>
2020-01-30 12:15:31 -0700

    Finished up the basic requirements for the team activity. Added a Reset button and changed some formatting.

---
+++ week04/w4_ta.js
@@ -0,0 +1,24 @@
+document.getElementById('tbl').addEventListener('touchend', onClk, false);
+//document.getElementById('rst').addEventListener('click')
+let turn = 0;
+function onClk(e){
+    console.log(e.target);
+    if(turn % 2 == 0){
+        e.target.innerHTML = "X";
+        turn+=1;
+    }
+    else {
+        e.target.innerHTML = "0";
+        turn+=1;
+    }
+
+}
+
+function reset(){
+    let tb = document.getElementById("tbl");
+    for(i = 0, n = tb.rows.length; i<n; i++){
+        for(c = 0, m = tb.rows[i].cells.length; c < m; c++){
+            tb.rows[i].cells[c].innerHTML ="";
+        }
+    }
+}
