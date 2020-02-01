663c837022c82758b25ee8c05d9e87650b86249e
nobleOn3 <nob16001@mybyui.edu>
2020-02-01 15:25:25 -0700

    This is where I actually added code for the PC (Player Character) builder I also added some to ToDo earlier, before this.

--- week04/w4_ex.js
+++ week04/w4_ex.js
@@ -1,3 +1,41 @@
+class PC {
+    constructor(clss="Barbarian", hp=10, nm="Name the Unknown") {
+        this.nm = nm;
+        this.clss = clss;
+        this.hp = hp;
+    }
+
+    washit(ht=0) {
+        this.hp -= ht;
+    }
+
+    display() {
+        console.log(this.nm);
+        console.log(this.hp);
+        console.log(this.clss);
+    }
+}
+
+function logPC() {
+    tmpPC = new PC(document.getElementById('clss').value, document.getElementById('hp').value, document.getElementById('nm').value)
+    tmpPC.display();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const input = document.forms[0].elements.sInput;
 input.addEventListener('blur', () => alert('blurred'), false);
 input.addEventListener('change', () => alert('Value has changed'), false);
