255238c4ea8990a932d275da64b77ddf687b1cc0
nobleOn3 <nob16001@byui.edu>
2020-04-04 18:47:04 -0600

    Added several new features, including combatant sorting by Initiative, combatants able to be attacked/healed, spells can be displayed and their duration reduced by moving to the next round

--- combat_tracker/main.js
+++ combat_tracker/main.js
@@ -12,12 +12,28 @@ document.getElementById('add_spell').addEventListener('touchend', () => {
     display_spells();
 });
 
+document.getElementById('next_round').addEventListener('touchend', () =>{
+    next_round();
+    display_spells();
+})
+
+document.getElementById('attack').addEventListener('touchend', () =>{
+    attack(document.getElementById('value').value);
+    display_combatants();
+})
+
+document.getElementById('heal').addEventListener('touchend', () =>{
+    heal(document.getElementById('value').value);
+    display_combatants();
+})
+
 //Lists of objects
 let c_list = []; //list of combatants
 let s_list = []; // list of spells
 
 //Display functions
 function display_combatants(){
+
     let c_display = document.getElementById("c_table");
 
     c_display.innerHTML = "";
@@ -61,8 +77,36 @@ function display_combatants(){
 }
 
 function display_spells(){
+    let s_display = document.getElementById("s_table");
+
+    s_display.innerHTML = "";
+
+    let newRow = document.createElement('tr');
+
+    let newCell = document.createElement('td');
+    newCell.innerHTML = "Name";
+    newRow.appendChild(newCell);
+
+    newCell = document.createElement('td');
+    newCell.innerHTML = "Duration";
+    newRow.appendChild(newCell);
+
+
+    s_display.appendChild(newRow); 
+
     for(const element of s_list) {
-        console.log(element.name);
+        console.log(element.name, element.duration);
+
+        newRow = document.createElement('tr');
+        newCell = document.createElement('td');
+        newCell.innerHTML = element.name;
+        newRow.appendChild(newCell);
+
+        newCell = document.createElement('td');
+        newCell.innerHTML = element.duration;
+        newRow.appendChild(newCell);
+
+        s_display.appendChild(newRow);       
     }
 }
 
@@ -82,7 +126,50 @@ function add_combatant(){
 
     let c = new Combatant(name, init, hp);
     c_list.push(c);
+    c_list.sort(compare_cmbt);
 }
+
+function compare_cmbt(a,b){
+    if(b.initiative < a.initiative)
+        return -1;
+    else if(b.intiative > a.intiative)
+        return 1;
+    else
+        return 0;
+}
+
+function next_round() {
+    for(const element of s_list){
+        console.log(element);
+        element.reduce_duration(1);
+        if(element.duration === 0){
+            let removeIndex = s_list.map(function(element){return element;});
+            s_list.splice(removeIndex, 1);
+        }
+    }
+}
+
+function attack(damage){
+    let combatant = c_list.find(checkCombatant);
+
+    combatant.getHit(damage);
+
+    if(combatant.hp === 0){
+        let removeIndex = c_list.map(function(combatant){return combatant;});
+        c_list.splice(removeIndex, 1);
+    }
+}
+
+function checkCombatant(combatant){
+    return combatant.name == document.getElementById('victim').value;
+}
+
+function heal(healing){
+    let combatant = c_list.find(checkCombatant);
+
+    combatant.getHealed(parseInt(healing));
+}
+
 
 
 
